d6b9da0da6d86d964023e4ec36353c3018453786
Your Name <you@example.com>
2025-12-11 19:29:04 +0100

    Add secrets

---
+++ ShowRandomImage/secrets_example.h
@@ -0,0 +1,12 @@
+// EXAMPLE SECRETS FILE - DO NOT COMMIT TO GIT
+// Copy this file to "secrets.h" and fill in your credentials
+// Make sure "secrets.h" is in your .gitignore file
+
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi credentials
+const char WIFI_SSID[] = "YOUR_SSID_HERE";
+const char WIFI_PASSWORD[] = "YOUR_PASSWORD_HERE";
+
+#endif
